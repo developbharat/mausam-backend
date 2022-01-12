3ff9e9e790e25285a3881b8d02c8acf2f3bf8767
Jayant Malik <dev.jayantmalik@gmail.com>
2022-01-12 23:32:56 +0530

    fix: restricted access permissions to admin only

--- src/modules/area/apollo/admin/DistrictResolver.ts
+++ src/modules/area/apollo/admin/DistrictResolver.ts
@@ -1,18 +1,18 @@
 import { Arg, Mutation, Resolver, UseMiddleware } from "type-graphql";
-import { isAuthenticated } from "../../../../apollo/middlewares/isAuthenticated";
+import { isAdmin } from "../../../../apollo/middlewares/isAdmin";
 import { District } from "../../../../entities/area/District";
 import * as districtService from "../../services/admin/district.service";
 import { CreateNewDistrictInput, DeleteDistrictInput } from "./district";
 
 @Resolver()
 export class DistrictResolver {
-  @UseMiddleware(isAuthenticated)
+  @UseMiddleware(isAdmin)
   @Mutation(() => District)
   async create_new_district(@Arg("options") options: CreateNewDistrictInput): Promise<District> {
     return await districtService.create_new_district(options);
   }
 
-  @UseMiddleware(isAuthenticated)
+  @UseMiddleware(isAdmin)
   @Mutation(() => District)
   async delete_district(@Arg("options") options: DeleteDistrictInput): Promise<District> {
     return await districtService.delete_district({ district_id: options.district_id });
--- src/modules/area/apollo/admin/StateResolver.ts
+++ src/modules/area/apollo/admin/StateResolver.ts
@@ -1,19 +1,19 @@
 import { Arg, Mutation, Resolver, UseMiddleware } from "type-graphql";
-import { isAuthenticated } from "../../../../apollo/middlewares/isAuthenticated";
+import { isAdmin } from "../../../../apollo/middlewares/isAdmin";
 import { State } from "../../../../entities/area/State";
 import * as stateService from "../../services/admin/state.service";
 import { CreateNewStateInput, DeleteStateInput } from "./state";
 
 @Resolver()
 export class StateResolver {
-  @UseMiddleware(isAuthenticated)
+  @UseMiddleware(isAdmin)
   @Mutation(() => State)
   async create_new_state(@Arg("options") options: CreateNewStateInput): Promise<State> {
     const user = await stateService.create_new_state(options);
     return user;
   }
 
-  @UseMiddleware(isAuthenticated)
+  @UseMiddleware(isAdmin)
   @Mutation(() => State)
   async delete_state(@Arg("options") options: DeleteStateInput): Promise<State> {
     return await stateService.delete_state({ state_id: options.state_id });
